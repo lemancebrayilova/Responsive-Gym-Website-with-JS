ae16613386decc1bd6efe2f015f4c20048ccfdf2
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-06-15 10:21:46 +0300

    scroll bar and scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,7 +56,13 @@ const scrollActive = () => {
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
 
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
